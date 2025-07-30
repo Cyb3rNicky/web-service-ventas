73e12d60423813eaa728e3c6a7b136b9b5c43d67
Allan Chopen <allanchopen@gmail.com>
2025-07-30 11:45:13 -0600

    Se agrego campos faltantes a controlador de productos y se añadio gitignore

---
+++ WebServiceVentas/Data/VentasDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebServiceVentas.Models;
+
+namespace WebServiceVentas.Data
+{
+    public class VentasDbContext : DbContext
+    {
+        public VentasDbContext(DbContextOptions<VentasDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ WebServiceVentas/Models/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WebServiceVentas.Models
+
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int cantidad { get; set; }
+
+        [Column(TypeName = "text")]
+        public string descripcion { get; set; }
+    }
+}
