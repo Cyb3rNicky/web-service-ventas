df3c07fe4d9ae9133d82930971db39b3f27e4463
Allan Chopen <66160815+AllanChopen@users.noreply.github.com>
2025-08-14 22:24:25 -0600

    Update Producto.cs

--- WebServiceVentas/Models/Producto.cs
+++ WebServiceVentas/Models/Producto.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 
 namespace WebServiceVentas.Models
 {
@@ -8,24 +7,20 @@ namespace WebServiceVentas.Models
     public class Producto
     {
         [Key]
-        [Column("Id")]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El nombre es obligatorio")]
-        [Column("Nombre")]
         public string Nombre { get; set; } = string.Empty;
 
         [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
-        [Column("Precio")]
+        [Column(TypeName = "numeric")]
         public decimal Precio { get; set; }
 
         [Range(0, int.MaxValue, ErrorMessage = "La cantidad no puede ser negativa")]
-        [Column("cantidad")]
         public int Cantidad { get; set; }
 
         [Required(ErrorMessage = "La descripción es obligatoria")]
-        [Column("descripcion", TypeName = "text")]
-        [JsonPropertyName("descripcion")]
+        [Column(TypeName = "text")]
         public string Descripcion { get; set; } = string.Empty;
     }
 }
