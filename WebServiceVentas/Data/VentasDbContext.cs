793c6fd51d6c8c6833d069970d62576c3940bc92
Allan Chopen <66160815+AllanChopen@users.noreply.github.com>
2025-08-14 21:59:13 -0600

    Update VentasDbContext.cs

--- WebServiceVentas/Data/VentasDbContext.cs
+++ WebServiceVentas/Data/VentasDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebServiceVentas.Models;
 
 namespace WebServiceVentas.Data
@@ -7,6 +7,23 @@ namespace WebServiceVentas.Data
     {
         public VentasDbContext(DbContextOptions<VentasDbContext> options) : base(options) { }
 
-        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Producto> Productos { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Producto>()
+                .ToTable("Productos");
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("Id");
+                entity.Property(e => e.Nombre).HasColumnName("Nombre");
+                entity.Property(e => e.Precio).HasColumnName("Precio");
+                entity.Property(e => e.Cantidad).HasColumnName("Cantidad");
+                entity.Property(e => e.Descripcion).HasColumnName("Descripcion");
+            });
+        }
     }
 }
